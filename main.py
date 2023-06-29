78ede512898c81c43db53b6033fea93a6e6afb28
Alex <124547242+vc08932@users.noreply.github.com>
2023-06-29 14:49:41 +0800

    Update main.py
    Add more choices

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
 import requests
-import calendar
 import datetime
+import sys
 
+def get_data():
+  res = requests.get("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=tc")
+  data=json.loads(res.text)
+  return data
 
 def dateprocess(str):
   year = str[0:4]
@@ -10,20 +14,60 @@ def dateprocess(str):
   day = str[6:9]
   return year,month,day
 
+def weather_info(date,full=0):
+  data = get_data()
+  if full == 0:
+    print("天氣概況：",data['generalSituation'],"\n")
 
-res = requests.get("https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=fnd&lang=tc")
-data=json.loads(res.text)
-
-print("天氣概況：",data['generalSituation'],"\n")
-for i in range(0,9):
-  year = int(dateprocess(data["weatherForecast"][i]["forecastDate"])[0])
-  month = int(dateprocess(data["weatherForecast"][i]["forecastDate"])[1])
-  day = int(dateprocess(data["weatherForecast"][i]["forecastDate"])[2])
+  year = int(dateprocess(data["weatherForecast"][date]["forecastDate"])[0])
+  month = int(dateprocess(data["weatherForecast"][date]["forecastDate"])[1])
+  day = int(dateprocess(data["weatherForecast"][date]["forecastDate"])[2])
 
   print(f'{year} 年 {month} 月 {day} 日 ({datetime.date(year,month,day).strftime( "%A")[:3]})')
-  print(f'風：{data["weatherForecast"][i]["forecastWind"]}')
-  print(f'天氣：{data["weatherForecast"][i]["forecastWeather"]}')
-  print(f'最高溫度：{data["weatherForecast"][i]["forecastMaxtemp"]["value"]}°C')
-  print(f'最低溫度：{data["weatherForecast"][i]["forecastMintemp"]["value"]}°C')
-  print(f'相對濕度：{data["weatherForecast"][i]["forecastMinrh"]["value"]}% - {data["weatherForecast"][i]["forecastMaxrh"]["value"]}% ')
+  print(f'風：{data["weatherForecast"][date]["forecastWind"]}')
+  print(f'天氣：{data["weatherForecast"][date]["forecastWeather"]}')
+  print(f'最高溫度：{data["weatherForecast"][date]["forecastMaxtemp"]["value"]}°C')
+  print(f'最低溫度：{data["weatherForecast"][date]["forecastMintemp"]["value"]}°C')
+  print(f'相對濕度：{data["weatherForecast"][date]["forecastMinrh"]["value"]}% - {data["weatherForecast"][date]["forecastMaxrh"]["value"]}% ')
   print("-"*20,"\n")
+
+def all_info():
+  for i in range(0,9):
+    weather_info(i,i)
+
+while True:
+  print("""
+  [0]: 退出程序;\n
+  [1]: 獲取 9 天全部天氣預報；\n
+  [2]: 獲取九天内指定日期的天氣預報；\n
+  提示：只需輸入數字部分。
+  """)
+  try:
+    command = int(input("指令："))
+
+  except:
+    continue
+  print()
+  
+
+  if command == 0:
+    sys.exit()
+  elif command == 1:
+    all_info()
+  elif command == 2:
+    try:
+      date = int(input("第幾天："))
+
+      while date <= 0 or date > 9:
+        print("請輸入 1-9\n")
+        date = int(input("第幾天："))
+
+    except:
+      print("請輸入 1-9\n")
+      continue
+
+    weather_info(date-1)
+
+  else:
+    print("請按指令輸入。")
+    continue
